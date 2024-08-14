a9f63fed45801f8148976a8203a87c4ca054d3c5
Luciano Terres <terres@petrobras.com.br>
2024-08-14 13:57:40 -0300

    Remove trailing spaces

--- main.py
+++ main.py
@@ -21,4 +21,60 @@ class Velan:
         print ('z   ', '  veloc')
         for i in range(len(self.zlist)):
             print(self.zlist[i], self.velocitylist[i])
+
+def load_data(velans):
+    # Python
+    filename = "velans1.txt"
+    # Open the file in read mode ('r')
+    with open(filename, 'r') as file:
+        # Read the content of the file
+        i = 0
+        for line in file:
+            line = line.rstrip()  # Remove trailing spaces
+            lineType = line[0]
+            if lineType == "S":
+                # extract coordinates;
+                coordX, coordY = line[25:35], line[36:45]
+                #print(lineType, coordX, coordY)
+                # Cria uma instância da classe Velan
+                velan_instance = Velan(coordX, coordY)
+                #crie uma lista de velans   
+                velans.append(velan_instance)
+                continue
+
+
+            if line[0] == "V":
+                lineSize = len(line)
+                for i in range(20, lineSize, 10):
+                    if i + 10 <= lineSize:
+                        z, velocity =  line[i:i+5],  line[i+6:i+10]
+                        #print(z, velocity)
+                        #get the last element of the list velans
+                        velan_instance = velans[-1]
+                        velan_instance.add(z,velocity)
+                        continue
+                    
+    return velans
+
+
+if __name__ == "__main__":
+    print ("Program Init")
+    # Create an instance of Velan
+    velan_instance = Velan(10, 20)
+
+    # Add some data
+    velan_instance.add(5, 100)
+    velan_instance.add(6, 150)
+
+    # Show the data
+    velan_instance.show()
+    
+    velans = []
+    load_data(velans)
+
+    for v in velans:
+        v.show()
+
+
+    
     
